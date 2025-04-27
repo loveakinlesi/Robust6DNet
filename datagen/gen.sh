247ee426e5087394b32b280a58f209512765fe4a
Love Akinlesi <akinlesil@gmail.com>
2025-04-27 19:25:31 +0100

    Update

--- datagen/gen.sh
+++ datagen/gen.sh
@@ -1,8 +1,8 @@
-python datagen/find_high_visib_images.py --obj_id 2
-python datagen/generate_annotations.py --obj_id 2
-python datagen/generate_test_annotations.py --obj_id 2
-python datagen/generate_crops.py --obj_id 2
-python datagen/generate_heatmaps.py --obj_id 2
-python training/train_lm_r6dnet.py --obj_id 2
+python datagen/find_high_visib_images.py --obj_id 8
+python datagen/generate_annotations.py --obj_id 8
+python datagen/generate_test_annotations.py --obj_id 8
+python datagen/generate_crops.py --obj_id 8
+python datagen/generate_heatmaps.py --obj_id 8
+python training/train_r6dnet.py --obj_id 8
 
 echo "complete"
